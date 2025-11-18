0152727e334f9638cf2eb014f6582e38ec2f8848
elFedaEZ <albornozfederico98@hotmail.com>
2025-11-18 16:22:45 -0300

    Agregar app config CSharp

--- GeneradorContraseñas/obj/Debug/net8.0-windows/GeneradorContraseñas.AssemblyInfo.cs
+++ GeneradorContraseñas/obj/Debug/net8.0-windows/GeneradorContraseñas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GeneradorContraseñas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+256ae8df84177fb0815dcfea81be613b3f18ef6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e2a4b5991caba9713d550e5b917475d5f506381")]
 [assembly: System.Reflection.AssemblyProductAttribute("GeneradorContraseñas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GeneradorContraseñas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
